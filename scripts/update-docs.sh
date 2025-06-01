cdf185142466a80ca864645c5e2fc2584a50ed9e
Snehil Shah <snehilshah.989@gmail.com>
2025-06-02 03:04:03 +0530

    build: use version agnostic source files
    Signed-off-by: Snehil Shah <snehilshah.989@gmail.com>

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -3,18 +3,13 @@
 # Install mdextract globally
 npm install -g mdextract
 
-# Read the control file and extract default version
-default_version=$(grep "default_version" pg_dispatch.control | sed "s/.*'\(.*\)'.*/\1/")
-
-echo "Found default version: $default_version"
-
-# Replace <docs> with include: pg_dispatch--{version}.sql in README.md
-sed -i "s/<docs>/include: pg_dispatch--$default_version.sql/g" README.md
+# Replace <docs> with include: pg_dispatch.sql in README.md
+sed -i "s/<docs>/include: pg_dispatch.sql/g" README.md
 
 # Run mdextract --update README.md
 mdextract --update README.md
 
 # Replace the include comments back to <docs>
-sed -i "s/include: pg_dispatch--$default_version\.sql/<docs>/g" README.md
+sed -i "s/include: pg_dispatch\.sql/<docs>/g" README.md
 
 echo "Documentation update complete!"
